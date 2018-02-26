1acd3315aa995d0b6c8b0d663ffacabef3269c10
Ahmed Hosny <i.ahmedhosny@gmail.com>
2018-02-26 14:23:48 -0500

    Allows file uploads for inference

--- data/conversion.py
+++ data/conversion.py
@@ -7,6 +7,8 @@ def convert(imgFile):
    img = Image.open(imgFile)
    img = img.resize((224,224))
    npArr = np.array(img)
+   print npArr.shape
    npArr = np.moveaxis(npArr, -1, 0)
-   npArr = npArr[np.newaxis,:]
+   npArr = npArr[np.newaxis,:].astype(np.float32)
+   print npArr.shape
    return npArr
--- data/run.py
+++ data/run.py
@@ -1,18 +1,47 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request
 from redis import Redis, RedisError
+from werkzeug import secure_filename
 import json
 from json2html import *
 import os
 import socket
+import wtforms as wtf
+from inference import infer
+
+# form
+class Average(wtf.Form):
+    filename   = wtf.FileField(validators=[wtf.validators.InputRequired()])
 
 # Connect to Redis
 redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
 
+# app and uploads
 app = Flask(__name__)
+UPLOAD_DIR = 'uploads/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_DIR
+ALLOWED_EXTENSIONS = set(['jpg'])
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-@app.route("/")
-def hello():
+def handle_request():
+    form = Average(request.form)
+    filename = None
+    result = "upload a file to get results"
+    if request.method == 'POST':
+        if request.files:
+            file = request.files[form.filename.name]
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+                result = infer(UPLOAD_DIR + filename)
+            else:
+                result = "wrong file format"
+    #
+    return form,result
+
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
     config_json = json.load(open("config.json"))
     title = config_json['publication']['title']
     config = json2html.convert(json =config_json)
@@ -20,16 +49,18 @@ def hello():
     height="auto">'
     thumbnail = '<img src="/static/thumbnail.png" alt="thumbnail" width="10%" \
     height="auto">'
-    form = '<form action=""> \
-      <input type="file" name="pic" accept="image/*"> \
-      <input type="submit"> \
-    </form>'
-    return render_template('index.html',
+    #
+    form, result = handle_request()
+    #
+    return render_template(
+    'index.html',
     title=title,
     config=config,
     schematic=schematic,
     thumbnail=thumbnail,
-    form=form)
+    form=form,
+    result=result
+    )
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
