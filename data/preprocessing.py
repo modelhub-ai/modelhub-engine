96829a7e0cde7288b54e08dabee5500bd8f6e594
michaelschwier <mschwier@bwh.harvard.edu>
2018-02-26 19:31:37 -0500

    converting everything to 3 color channels

--- data/preprocessing.py
+++ data/preprocessing.py
@@ -1,5 +1,13 @@
 # preprocess.py
 # preprocesses numpy arrays according to contributors' pipeline
+import numpy as np
+
 def preprocess(arr):
+    if arr.shape[1] > 3:
+        arr = arr[:,0:3,:,:]
+    elif arr.shape[1] < 3:
+        arr = arr[:,[0],:,:]
+        arr = np.concatenate((arr, arr[:,[0],:,:]), axis = 1)
+        arr = np.concatenate((arr, arr[:,[0],:,:]), axis = 1)
     print ('preprocessing done.')
     return arr
