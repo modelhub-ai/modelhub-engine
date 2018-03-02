52ab3babdb098238864f89ac88fb4086c082f0c3
Michael Schwier <mschwier@bwh.harvard.edu>
2018-03-02 15:33:58 -0500

    Added documentation

--- data/modelhublib/imageloaders/imageLoader.py
+++ data/modelhublib/imageloaders/imageLoader.py
@@ -15,6 +15,10 @@ class ImageLoader(object):
 
 
     def load(self, input):
+        """
+        Tries to load input and on fail forwards load request to next handler
+        until sucess or final fail.
+        """
         try:
             image = self._load(input)
         except:
@@ -22,17 +26,29 @@ class ImageLoader(object):
                 return self._sucessor.load()
             else:
                 if isinstance(input, six.string_types):
-                    raise IOError("No handler was able to load the file \"%s\"." % input)
+                    raise IOError("Was not able to load the file \"%s\"." % input)
                 else:
-                    raise IOError("No handler was able to load input of type \"%s\"." % type(input).__name__)
+                    raise IOError("Was not able to load input of type \"%s\"." % type(input).__name__)
         self._checkConfigCompliance(image)
         return image
 
 
     def _load(self, input):
+        """
+        Loads and return the image of given input.
+
+        When overwriting this, make sure to raise IOError if input cannot
+        be loaded.
+        """
         raise NotImplementedError("This is a method of an abstract class.")
 
 
     def _checkConfigCompliance(self, image):
+        """
+        Check if image complies with configuration.
+
+        When overwriting this, make sure to raise IOError if image does
+        not comply with config.
+        """
         raise NotImplementedError("This is a method of an abstract class.")
 
