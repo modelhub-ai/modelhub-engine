3fbe96bc41e626e7fab0c36316792237706a46a7
Ahmed Hosny <i.ahmedhosny@gmail.com>
2018-04-09 12:20:10 -0400

    bypasses netron

--- usr_src/run.py
+++ usr_src/run.py
@@ -3,7 +3,5 @@ import netron
 import sys
 
 if __name__ == "__main__":
-    if sys.argv[1] == "netron":
-        netron.serve_file("/usr_src/model/model.onnx", port=81, host="0.0.0.0")
-    else:
+        #netron.serve_file("/usr_src/model/model.onnx", port=81, host="0.0.0.0")
         webservice.start()
