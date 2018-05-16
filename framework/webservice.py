97cbfb6feb6ed2ad0befa5cb87ba35a83090507c
Ahmed Hosny <i.ahmedhosny@gmail.com>
2018-05-15 22:18:07 -0400

    adds ability to send images to the browser

--- framework/webservice.py
+++ framework/webservice.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, send_from_directory, jsonify
+from flask import Flask, render_template, request, send_from_directory, jsonify, send_file
 from urllib import unquote
 from redis import Redis, RedisError
 import json
@@ -7,6 +7,9 @@ import socket
 from inference import infer
 from datetime import datetime
 import pprint
+from PIL import Image
+import numpy as np
+from StringIO import StringIO
 
 # https://stackoverflow.com/questions/25466904/print-raw-http-request-in-flask-or-wsgi
 class LoggingMiddleware(object):
@@ -50,6 +53,13 @@ def index():
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
+# https://stackoverflow.com/questions/7877282/how-to-send-image-generated-by-pil-to-browser
+def serve_pil_image(pil_img):
+    img_io = StringIO()
+    pil_img.save(img_io, 'JPEG', quality=100)
+    img_io.seek(0)
+    return send_file(img_io, mimetype='image/jpeg')
+
 # route for getting predictions via file upload
 @app.route('/predict', methods=['POST'])
 def upload1():
@@ -63,23 +73,32 @@ def upload1():
             file.save(filename)
             try:
                 result = infer(filename)
+                if isinstance(result, Image.Image):
+                    result = serve_pil_image(result)
+                else:
+                    result = jsonify(result=result)
             except Exception as e:
                 result = "ERROR: " + str(e)
-            return jsonify(result=result)
+            return result
 
-# route for getting predictions via url - only for our sample_data
-# WARNING: only safely works for our sample data - for other urls, we need more
+# WARNING: route for getting predictions via url - only for our sample_data
+#  only safely works for our sample data - for other urls, we need more
 # GET with "content-type" in header
 # https://stackoverflow.com/questions/4776924/how-to-safely-get-the-file-extension-from-a-url
+#
 @app.route('/predict_sample', methods=['GET'])
 def upload2():
     if request.method == 'GET':
         filename = request.args.get('filename')
         try:
             result = infer("../contrib_src/sample_data/" + filename)
+            if isinstance(result, Image.Image):
+                result = serve_pil_image(result)
+            else:
+                result = jsonify(result=result)
         except Exception as e:
             result = "ERROR: " + str(e)
-        return jsonify(result=result)
+        return result
 
 # routing for figures that exist in the contrib_src - model thumbnail
 @app.route('/model/figures/<figureName>')
