5954d91230a28986cbbd6b821b84d66f266d8c17
Michael Schwier <mschwier@bwh.harvard.edu>
2018-05-21 13:59:32 -0400

    Fixed URL to match new Docker

--- framework/static/js/globalVar.js
+++ framework/static/js/globalVar.js
@@ -1,4 +1,9 @@
 let windowLocation = window.location.host;
 let split = window.location.href.split(":");
-let appUrl = split[0] + ":" + split[1] + ":" + "4000/";
-let netronUrl = split[0] + ":" + split[1] + ":" + "4001/";
+if (split[1].slice(-1) == "/") {
+    split[1] = split[1].slice(0, -1)
+}
+let appUrl = split[0] + ":" + split[1] + ":" + "80/";
+let netronUrl = split[0] + ":" + split[1] + ":" + "81/";
+console.log(split)
+console.log(netronUrl)
