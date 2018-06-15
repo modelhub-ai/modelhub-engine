b8dcaae67221e48188c9e7f71dcf5eb4240c3528
Michael Schwier <mschwier@bwh.harvard.edu>
2018-06-15 17:10:22 -0400

    Mock model for api tests

---
+++ framework/modelhubapi_tests/mockmodel/contrib_src/inference.py
@@ -0,0 +1,13 @@
+from modelhublib.model import ModelBase
+
+
+class Model(ModelBase):
+
+    def __init__(self):
+        pass    
+
+    def infer(self, input):
+        output = None
+        return output
+        
+
