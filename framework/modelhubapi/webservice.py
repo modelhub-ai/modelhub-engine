66819538b8ffb3078511ce3c82b25535811ea9fd
Ahmed Hosny <i.ahmedhosny@gmail.com>
2018-06-18 15:35:33 -0400

    Revert "deletes old webservice"
    This reverts commit c1293e8a68eff51ccdd871c893d20f7a30b00606.

--- framework/modelhubapi/webservice.py
+++ framework/modelhubapi/webservice.py
@@ -36,23 +36,12 @@ def _startWebservice(model, contribSrcDir):
 
 # temporary tests for ModelHubAPI (python)
 def _testModelHubAPI(model, contribSrcDir):
-    # tests for python API
     api = ModelHubAPI(model, contribSrcDir)
     _testHelper(api.get_config(), 'get_config')
     _testHelper(api.get_legal(), 'get_legal')
     _testHelper(api.get_model_io(), 'get_model_io')
     _testHelper(api.get_samples(), 'get_samples')
     _testHelper(api.predict("/contrib_src/sample_data/house.jpg"), 'predict')
-    # tests for REST API (through command line - I am mapping 80 to 4000)
-    # curl -i http://localhost:4000/api/v1.0/get_config
-    # curl -i http://localhost:4000/api/v1.0/get_legal
-    # curl -i http://localhost:4000/api/v1.0/get_model_io
-    # curl -i http://localhost:4000/api/v1.0/get_model_files
-    # curl -i http://localhost:4000/api/v1.0/get_samples
-    # curl -i http://localhost:4000/api/v1.0/get_thumbnail
-    # these are testing on the same image, one through URL and the other through an upload. We can assert the the output from both is identical
-    # curl -i http://localhost:4000/api/v1.0/predict?fileurl=https://github.com/modelhub-ai/modelhub/raw/master/squeezenet/contrib_src/sample_data/trump.jpg
-    # curl -i -X POST -F file=@/home/ahmed/Dropbox/DFCI/14_zoo/modelhub/squeezenet/contrib_src/sample_data/trump.jpg "http://localhost:4000/api/v1.0/predict"
 
 def _testHelper(func, name):
     print "TESTING ", name
