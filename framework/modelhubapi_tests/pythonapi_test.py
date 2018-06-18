efc54056e4074d8ec559e7986a98f334fe0571f2
Michael Schwier <mschwier@bwh.harvard.edu>
2018-06-18 14:14:18 -0400

    Using common api test base class now
    reduces duplicated assert code

--- framework/modelhubapi_tests/pythonapi_test.py
+++ framework/modelhubapi_tests/pythonapi_test.py
@@ -1,10 +1,10 @@
-import unittest
+from apitestbase import TestAPIBase
 import os
 from modelhubapi import ModelHubAPI
 from modelhubapi_tests.mockmodel.contrib_src.inference import Model
 
 
-class TestModelHubAPI(unittest.TestCase):
+class TestModelHubAPI(TestAPIBase):
 
     def setUp(self):
         model = Model()
@@ -24,37 +24,22 @@ class TestModelHubAPI(unittest.TestCase):
 
     def test_get_config_returns_correct_dict(self):
         config = self.api.get_config()
-        self.assertEqual("MockId", config["id"])
-        self.assertEqual("MockNet", config["meta"]["name"])
+        self.assert_config_contains_correct_dict(config)
     
 
     def test_get_legal_returns_expected_keys(self):
         legal = self.api.get_legal()
-        self.assertTrue("error" not in legal)
-        keys = legal.keys()
-        keys.sort()
-        referenceKeys = ["model_license", 
-                         "modelhub_acknowledgements", 
-                         "modelhub_license", 
-                         "sample_data_license"]
-        self.assertListEqual(referenceKeys, keys)
+        self.assert_legal_contains_expected_keys(legal)
     
 
     def test_get_legal_returns_expected_mock_values(self):
         legal = self.api.get_legal()
-        self.assertEqual("TEST MODEL LICENSE", legal["model_license"])
-        self.assertEqual("TEST SAMPLE DATA LICENSE", legal["sample_data_license"])
+        self.assert_legal_contains_expected_mock_values(legal)
     
 
     def test_get_model_io_returns_expected_mock_values(self):
         model_io = self.api.get_model_io()
-        self.assertListEqual(["image/png"], model_io["input"]["format"])
-        self.assertEqual(1, model_io["input"]["dim_limits"][0]["min"])
-        self.assertEqual(4, model_io["input"]["dim_limits"][0]["max"])
-        self.assertEqual(1, model_io["input"]["dim_limits"][1]["min"])
-        self.assertEqual(1, model_io["input"]["dim_limits"][2]["min"])
-        self.assertEqual("probabilities", model_io["output"][0]["name"])
-        self.assertEqual("label_list", model_io["output"][0]["type"])
+        self.assert_model_io_contains_expected_mock_values(model_io)
 
 
     def test_get_samples_returns_path_to_mock_samples(self):
@@ -65,18 +50,12 @@ class TestModelHubAPI(unittest.TestCase):
     
     def test_predict_returns_expected_mock_prediction(self):
         result = self.api.predict("MOCK_MODEL_NEEDS_NO_INPUT_FILE")
-        self.assertEqual("class_0", result["output"][0]["label"])
-        self.assertEqual(0.3, result["output"][0]["probability"])
-        self.assertEqual("class_1", result["output"][1]["label"])
-        self.assertEqual(0.7, result["output"][1]["probability"])
+        self.assert_predict_contains_expected_mock_prediction(result)
     
 
     def test_predict_returns_expected_mock_meta_info(self):
         result = self.api.predict("MOCK_MODEL_NEEDS_NO_INPUT_FILE")
-        self.assertEqual("label_list", result["output_type"])
-        self.assertEqual("probabilities", result["output_name"])
-        self.assertEqual("MockId", result["model"]["id"])
-        self.assertEqual("MockNet", result["model"]["name"])
+        self.assert_predict_contains_expected_mock_meta_info(result)
 
 
 
