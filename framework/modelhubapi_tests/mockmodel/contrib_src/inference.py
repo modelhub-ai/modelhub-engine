3f2076bfe59fa74e540ff567d6263191d914a24c
Michael Schwier <mschwier@bwh.harvard.edu>
2018-07-25 11:37:03 -0400

    Added various mock models for testing different prediction result flavours

--- framework/modelhubapi_tests/mockmodel/contrib_src/inference.py
+++ framework/modelhubapi_tests/mockmodel/contrib_src/inference.py
@@ -1,3 +1,13 @@
+"""
+Implementation of several mock models to test the API. Each model has a 
+slightly different behaviour, which should be properly handled by the API.
+Also most models are not fully valid, e.g. they do not comply to the mock
+config. This is ok for unit testing, most models are only used for a small
+set of specific tests requiring that model's specific behavioural aspect.
+"""
+
+import os
+import numpy as np
 from modelhublib.model import ModelBase
 
 
@@ -7,11 +17,56 @@ class Model(ModelBase):
         pass    
 
     def infer(self, input):
-        # Return a constant classification result no matter what's the input
-        output = [{"label": "class_0", 'probability': 0.3},
-                  {"label": "class_1", 'probability': 0.7}]
-        return output
-        
+        if os.path.isfile(input):
+            # Return a constant classification result no matter what's the input
+            label_list = [{"label": "class_0", 'probability': 0.3},
+                          {"label": "class_1", 'probability': 0.7}]
+            mask = np.asarray([[0,1,1,0],[0,2,2,0]])
+            return [label_list, mask]
+        else:
+            raise IOError("File " + input + " does not exist.")
+
+
+class ModelReturnsOneNumpyArray(ModelBase):
+
+    def __init__(self):
+        pass    
+
+    def infer(self, input):
+        if os.path.isfile(input):
+            # Return a constant classification result no matter what's the input
+            mask = np.asarray([[0,1,1,0],[0,2,2,0]])
+            return mask
+        else:
+            raise IOError("File " + input + " does not exist.")
+
+
+class ModelReturnsListOfOneNumpyArray(ModelReturnsOneNumpyArray):
+
+    def infer(self, input):
+        return [super(ModelReturnsListOfOneNumpyArray, self).infer(input)]
+
+
+class ModelReturnsOneLabelList(ModelBase):
+
+    def __init__(self):
+        pass    
+
+    def infer(self, input):
+        if os.path.isfile(input):
+            # Return a constant classification result no matter what's the input
+            label_list = [{"label": "class_0", 'probability': 0.3},
+                          {"label": "class_1", 'probability': 0.7}]
+            return label_list
+        else:
+            raise IOError("File " + input + " does not exist.")
+
+
+class ModelReturnsListOfOneLabelList(ModelReturnsOneLabelList):
+
+    def infer(self, input):
+        return [super(ModelReturnsListOfOneLabelList, self).infer(input)]
+
 
 class ModelThrowingError(ModelBase):
 
