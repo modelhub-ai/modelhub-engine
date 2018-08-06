fb27b5d8ef6865e474f21df7870f13c333f20bec
Michael Schwier <mschwier@bwh.harvard.edu>
2018-08-06 15:00:22 -0400

    Removed old obsolete web frontend

--- framework/static/js/globalVar.js
+++
@@ -1,9 +0,0 @@
-let windowLocation = window.location.host;
-let split = window.location.href.split(":");
-if (split[1].slice(-1) == "/") {
-    split[1] = split[1].slice(0, -1)
-}
-let appUrl = split[0] + ":" + split[1] + ":" + "80/";
-let netronUrl = split[0] + ":" + split[1] + ":" + "81/";
-console.log(split)
-console.log(netronUrl)
